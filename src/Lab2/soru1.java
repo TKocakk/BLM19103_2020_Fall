51d2ecad508c003d573e24789c97052677567c40
Tuğçe Koçak <72867097+TKocakk@users.noreply.github.com>
2020-10-20 16:06:17 +0300

    soru1

--- src/Lab2/soru1.java
+++ src/Lab2/soru1.java
@@ -1,16 +1,16 @@
-
 package Lab2;
 
 public class soru1 {
-    public static void main(String[] args) {
-        
-        int sayi=3;
-        int sayi2=5;
-       // System.out.println(sayi+" "+sayi2+"");
-        System.out.println("Merhaba"+sayi+sayi);
-        System.out.println("Merhaba"+(sayi+sayi));
 
-      int f0 = 0, f1 = 1, f2;
+    public static void main(String[] args) {
+
+        int sayi = 3;
+        int sayi2 = 5;
+        // System.out.println(sayi+" "+sayi2+"");
+        System.out.println("Merhaba" + sayi + sayi);
+        System.out.println("Merhaba" + (sayi + sayi));
+
+        int f0 = 0, f1 = 1, f2;
         f2 = f1 + f0;
         System.out.println("f0:" + f0 + " f1:" + f1);
         // f0=0
@@ -21,19 +21,18 @@ public class soru1 {
         f2 = f1 + f0; //f2=2
 
         System.out.println("f0:" + f0 + " f1:" + f1);
-        
+
         f0 = f1;  //f0=1
         f1 = f2;   // f1=2
         f2 = f1 + f0;  //f2= 3
 
         System.out.println("f0:" + f0 + " f1:" + f1);
-        
+
         f0 = f1;   //f0=2
         f1 = f2;   //f1=3
         f2 = f1 + f0;   //f2=5
 
-        System.out.println("f0:" + f0 + " f1:" + f1 +" f2:" +f2);
-
+        System.out.println("f0:" + f0 + " f1:" + f1 + " f2:" + f2);
 
     }
 }
